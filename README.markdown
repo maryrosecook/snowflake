d05f8e121a5daca92aecdf7ca141102689c58c22
Mary Rose Cook <maryrosecook@maryrosecook.com>
2011-08-14 14:58:39 +0100

    first

---
+++ README.markdown
@@ -0,0 +1,50 @@
+#Snowflake.js
+Run code in beautiful and unique situations
+http://snowflakejs.maryrosecook.com
+
+By mary rose cook
+http://maryrosecook.com
+maryrosecook@maryrosecook.com
+
+##What is Snowflake.js?
+
+Snowflake lets you run a piece of code once, or every two seconds, or when a value changes, or when it stops changing.  It is useful for debugging a function that is run very frequently.
+
+<pre><code>
+  // import Snowflake
+
+  var s = new Snowflake();
+  var problemFunc = function() {
+    var interestingVar = 0;
+    // insert function machinations here
+
+    // print out interestingVar once, even if problemFunc is run more than once
+    s.once(function() {
+      console.log(interestingVar);
+    });
+
+    // print out interestingVar every two seconds
+    s.every(2, function() {
+      console.log(interestingVar);
+    });
+
+    // print out interestingVar if it has not changed since the last run
+    s.stuck([interestingVar], function() {
+      console.log(interestingVar);
+    });
+  }
+</code></pre>
+
+##Licence
+
+The code is open source, under the MIT licence.
+
+##Getting started
+
+* Download the repository from http://github.com/maryrosecook/snowflakejs
+
+* Import snowflake.js into your code.
+
+* Instantiate and use Snowflake as above.
+
+* See index.html for documentation.
---
+++ snowflake.js
@@ -0,0 +1,116 @@
+/*
+  Snowflake.js
+  by mary rose cook
+  http://github.com/maryrosecook/snowflakejs
+
+  Run code in beautiful and unique situations.
+*/
+
+;(function() {
+  function Snowflake() {
+    this.actions = {};
+  };
+
+  Snowflake.prototype = {
+    // runs the passed action once, passing in an optional arg
+    once: function(action, arg) {
+      var id = this.id(action);
+      if(this.actions[id] === undefined)
+      {
+        this.run(action, arg);
+        this.actions[id] = "whatever";
+      }
+    },
+
+    // runs action every interval seconds, passing optional arg
+    every: function(interval, action, arg) {
+      var id = this.id(action);
+      if(this.actions[id] === undefined)
+      {
+        this.actions[id] = {
+          nextRun: this.calculateNextRun(interval)
+        };
+      }
+      else if(this.actions[id].nextRun < new Date().getTime())
+      {
+        this.run(action, arg);
+        this.actions[id].nextRun = this.calculateNextRun(interval);
+      }
+    },
+
+    // runs action when watchValues stop changing, passing optional arg
+    stuck: function(watchValues, action, arg) {
+      this.watchedValueAction(watchValues, function(orig, current) {
+        for(var i = 0; i < orig.length; i++)
+          if(orig[i] !== current[i])
+            return false;
+
+        return true;
+      }, action, arg);
+    },
+
+    // runs action when watchValues start changing, passing optional arg
+    change: function(watchValues, action, arg) {
+      this.watchedValueAction(watchValues, function(orig, current) {
+        for(var i = 0; i < orig.length; i++)
+          if(orig[i] !== current[i])
+            return true;
+
+        return false;
+      }, action, arg);
+    },
+
+    // pretend like this action was never added
+    reset: function(action) {
+      delete this.actions[this.id(action)];
+    },
+
+    // takes function, stringifies it and turns it into a hash to create an identifier
+    id: function(action) {
+      return this.hashString(action.toString());
+    },
+
+    run: function(action, arg) {
+      action(arg);
+    },
+
+    calculateNextRun: function(interval) {
+      return new Date().getTime() + (interval * 1000);
+    },
+
+    watchedValueAction: function(watchValues, watchValueTest, action, arg) {
+      var id = this.id(action);
+      if(this.actions[id] === undefined)
+        this.actions[id] = { watchValues: watchValues };
+      else if(this.actions[id].spent !== true)
+      {
+        if(watchValueTest(this.actions[id].watchValues, watchValues) === true)
+        {
+          this.run(action, arg);
+          this.actions[id].spent = true;
+        }
+
+        // update watchValues
+        this.actions[id].watchValues = watchValues;
+      }
+    },
+
+    // taken from: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+    hashString: function(str) {
+      var hash = 0;
+	  if(str.length === 0)
+        return hash;
+
+	  for (i = 0; i < str.length; i++)
+      {
+	    var char = str.charCodeAt(i);
+	    var hash = ((hash << 5) -hash ) + char;
+	    hash = hash & hash;
+	  }
+
+	  return hash;
+    },
+  };
+
+  this.Snowflake = Snowflake;
+}).call(this);
