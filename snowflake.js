5912b494ddda6ae29d68f50c86cec0ea9a247130
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-22 14:06:28 -0400

    Make helper fns private.

--- snowflake.js
+++ snowflake.js
@@ -6,54 +6,54 @@
   Snowflake.prototype = {
     // runs the passed action once, passing in an optional arg
     once: function(action, arg) {
-      var id = this.id(action);
+      var id = makeId(action);
       if(this.actions[id] === undefined) {
         this.actions[id] = "whatever";
-        return this.run(action, arg);
+        return run(action, arg);
       }
     },
 
     // runs action every interval seconds, passing optional arg
     every: function(interval, action, arg) {
-      var id = this.id(action);
+      var id = makeId(action);
       if(this.actions[id] === undefined) {
         this.actions[id] = {
-          nextRun: this.calculateNextRun(interval)
+          nextRun: calculateNextRun(interval)
         };
       } else if(this.actions[id].nextRun < new Date().getTime()) {
-        this.actions[id].nextRun = this.calculateNextRun(interval);
-        return this.run(action, arg);
+        this.actions[id].nextRun = calculateNextRun(interval);
+        return run(action, arg);
       }
-    },
+    }
+  };
 
-    // takes function, stringifies it and turns it into a hash to create an identifier
-    id: function(action) {
-      return this.hashString(action.toString());
-    },
+  // taken from: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+  var hashString = function(str) {
+    var hash = 0;
+	  if(str.length === 0)
+      return hash;
 
-    run: function(action, arg) {
-      // maybe if action is not function then just console.log
-      return action(arg);
-    },
+	  for (i = 0; i < str.length; i++) {
+	    var char = str.charCodeAt(i);
+	    var hash = ((hash << 5) -hash ) + char;
+	    hash = hash & hash;
+	  }
 
-    calculateNextRun: function(interval) {
-      return new Date().getTime() + interval;
-    },
+	  return hash;
+  };
 
-    // taken from: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
-    hashString: function(str) {
-      var hash = 0;
-	    if(str.length === 0)
-        return hash;
+  // takes function, stringifies it and turns it into a hash to create an identifier
+  var makeId = function(action) {
+    return hashString(action.toString());
+  };
 
-	    for (i = 0; i < str.length; i++) {
-	      var char = str.charCodeAt(i);
-	      var hash = ((hash << 5) -hash ) + char;
-	      hash = hash & hash;
-	    }
+  var run = function(action, arg) {
+    // maybe if action is not function then just console.log
+    return action(arg);
+  };
 
-	    return hash;
-    },
+  var calculateNextRun = function(interval) {
+    return new Date().getTime() + interval;
   };
 
   this.Snowflake = Snowflake;
