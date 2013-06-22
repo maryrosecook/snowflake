a34da1c0af0662f2d01f0232c5e18f4fd6186b86
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-22 14:00:43 -0400

    Whitespace.

--- snowflake.js
+++ snowflake.js
@@ -88,17 +88,17 @@
     // taken from: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
     hashString: function(str) {
       var hash = 0;
-	  if(str.length === 0)
+	    if(str.length === 0)
         return hash;
 
-	  for (i = 0; i < str.length; i++)
+	    for (i = 0; i < str.length; i++)
       {
-	    var char = str.charCodeAt(i);
-	    var hash = ((hash << 5) -hash ) + char;
-	    hash = hash & hash;
-	  }
+	      var char = str.charCodeAt(i);
+	      var hash = ((hash << 5) -hash ) + char;
+	      hash = hash & hash;
+	    }
 
-	  return hash;
+	    return hash;
     },
   };
 
